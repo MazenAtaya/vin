58c846e88c493ed935131c44038b71ce301bdbc4
mataya <mataya@cleversafe.com>
2015-08-04 15:07:49 -0500

    Minor changes. Make a trivial test pass. Add require statement. add bundler.install statement

--- lib/vin.rb
+++ lib/vin.rb
@@ -1,5 +1,6 @@
 require "vin/version"
-
+require "vin/address"
+require "vin/subscriber"
 module Vin
   # Your code goes here...
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'bundler/setup'
+Bundler.setup
+
 require 'vin'
