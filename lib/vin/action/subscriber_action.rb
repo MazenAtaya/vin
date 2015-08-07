04a5162b5cdb9a69e4476d8a9a48b4c6c9e6c5f2
mataya <mataya@cleversafe.com>
2015-08-06 19:31:50 -0500

    add edit_sub method and its tests

--- lib/vin/action/subscriber_action.rb
+++ lib/vin/action/subscriber_action.rb
@@ -14,5 +14,32 @@ class SubsciberAction
 
   end
 
+  def edit_sub (subscribers, id, sub)
+    found_it = false
+    errors = []
+    subscribers.each do |subscriber|
+      if subscriber.id == id
+        found_it = true
+        errors = Validator.validate_sub(sub)
+        if errors.length == 0
+          subscriber.name = sub.name
+          subscriber.email = sub.email
+          subscriber.address = sub.address
+          subscriber.phone = sub.phone
+          subscriber.facebook = sub.facebook
+          subscriber.twitter = sub.twitter
+          break;
+        end
+      end
+    end
+
+    if !found_it
+      errors = [(Error.new 8, "Could not locate the subscriber with the given id")]
+    end
+
+    {'errors' => errors}
+
+  end
+
 
 end
--- spec/subscriber_action_spec.rb
+++ spec/subscriber_action_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'pp'
 describe SubsciberAction do
   before (:each) do
     @addess = Address.new "244 W 31st", "Chicago", "IL", "60661"
@@ -7,7 +8,6 @@ describe SubsciberAction do
     @invalid_sub_2 = Subscriber.new "Mazen", "mataya", @addess, "7734922211"
     @invalid_sub_3 = Subscriber.new "Mazen", "mataya@hawk.iit.edu", nil, "7734922211"
     @invalid_sub_4 = Subscriber.new "Mazen", "mataya@hawk.iit.edu", @addess, "Hello"
-
     @subs = Array.new
     @sub_action = SubsciberAction.new
   end
@@ -54,6 +54,40 @@ describe SubsciberAction do
     expect(@subs.length).to eq(0)
     expect(response['errors'].length).to eq(1)
   end
+
+  it 'should edit the subscriber with the given id if the information is valid' do
+    subs = [@sub]
+    edit_sub = Subscriber.new "new_Mazen", "motaya@hawk.iit.edu", @sub.address, "7734922212"
+    response = @sub_action.edit_sub(subs, @sub.id, edit_sub)
+    expect(response['errors'].length).to eq(0)
+    expect(subs[0].name).to eq("new_Mazen")
+    expect(subs[0].email).to eq("motaya@hawk.iit.edu")
+    expect(subs[0].phone).to eq("7734922212")
+    expect(subs[0].address).to eq(@sub.address)
+  end
+
+  it 'should give an error when the id is not valid' do
+    subs = [@sub]
+    edit_sub = Subscriber.new "new_Mazen", "motaya@hawk.iit.edu", @sub.address, "7734922212"
+    response = @sub_action.edit_sub(subs, @sub.id + 1, edit_sub)
+    expect(response['errors'].length).to eq(1)
+    expect(subs[0].name).to eq(@sub.name)
+    expect(subs[0].email).to eq(@sub.email)
+    expect(subs[0].phone).to eq(@sub.phone)
+    expect(subs[0].address).to eq(@sub.address)
+  end
+
+  it 'should give an error when the information is not valid' do
+    subs = [@sub]
+    edit_sub = Subscriber.new "", "motaya@hawk.iit.edu", @sub.address, "7734922212"
+    response = @sub_action.edit_sub(subs, @sub.id, edit_sub)
+    expect(response['errors'].length).to eq(1)
+    expect(subs[0].name).to eq(@sub.name)
+  end
+
+
+
+
 
 
 
