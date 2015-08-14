1d048719518d7b679a2ad50bf88da751920490bc
mataya <mataya@cleversafe.com>
2015-08-13 21:56:08 -0500

    require wine.rb

--- lib/vin.rb
+++ lib/vin.rb
@@ -2,6 +2,7 @@ require "vin/version"
 require "vin/model/address"
 require "vin/model/subscriber"
 require 'vin/model/error'
+require 'vin/model/wine'
 require 'vin/validator/validator'
 require 'vin/action/subscriber_action'
 
