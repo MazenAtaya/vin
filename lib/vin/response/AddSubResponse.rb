56a490f07927e7dbb438fd0f5e17a79b7fbe02d9
mataya <mataya@cleversafe.com>
2015-09-12 21:55:47 -0500

    delete an old response object and added html to git ignore

--- lib/vin/response/AddSubResponse.rb
+++
@@ -1,9 +0,0 @@
-class AddSubResponse
-attr_accessor :id, :errors
-
-  def initialize(id, errors)
-    @id = id
-    @errors = errors
-  end
-
-end
--- lib/vin_server.rb
+++ lib/vin_server.rb
@@ -37,13 +37,6 @@ end
 post '/vin/sub' do
   sub = parse_json request.body
   response = wc.subscriber_action.add_sub(wc.subscribers, sub);
-  ship = Shipment.new :AUG, "2015"
-  ship.status = :Delivered
-  ship1 = Shipment.new :JAN, "2015"
-  ship1.status = :Pending
-  ship.wines = [Wine.new, Wine.new, Wine.new]
-  #wc.subscribers[0].add_shipment ship
-  #wc.subscribers[0].add_shipment ship1
 
   if response['id'] != ""
     status 201
